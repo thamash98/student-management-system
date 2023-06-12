29dd19fee21366baf49ac17d1900bdf19b71815a
thamash98 <thasaranga123@gmail.com>
2023-06-12 08:40:55 +0530

    create student form

--- src/main/java/com/example/studentmanagementsystem/cotroller/StudentController.java
+++ src/main/java/com/example/studentmanagementsystem/cotroller/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.studentmanagementsystem.cotroller;
 
+import com.example.studentmanagementsystem.entity.Student;
 import com.example.studentmanagementsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,5 +20,12 @@ public class StudentController {
     public String studentList(Model model){
         model.addAttribute("students",studentService.getAllStudents());
         return "students";
+    }
+    @GetMapping("students/new")
+    public String createStudentForm(Model model){
+        Student student = new Student();//create student object to hold student form data
+        model.addAttribute("student",student);
+        return "create-student";
+
     }
 }
